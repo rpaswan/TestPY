ee273ddcda1b44ee6d73fc06a0cf9d4d2b1b79a2
rpaswan <roshanpaswan303@gmail.com>
2019-02-11 20:38:57 +0530

    Detailed about List in Python

---
+++ list_comprehension.py
@@ -0,0 +1,29 @@
+# num=[1,2,3,4,5,6,7]
+# even=[]
+# for i in num:
+#     if i%2==0:
+#         even.append(i)
+# print(even)
+
+# num=[1,2,3,4,5,6,7]
+# even=[i for i in num if i%2==0]
+# print(even)
+
+
+
+# x=input("enter any number")
+# x=int(x)
+# if x in num:
+#     print(x, "is found in list")
+
+
+# cities=["mumbai","new your", "paris"]
+# countary=["india", "USA", "france"]
+
+# z=zip(cities,countary)
+# print(z)
+# for i in z:
+#     print(i)
+
+box={"mumbai","new your", "paris"}
+print("mumbai" in box)
